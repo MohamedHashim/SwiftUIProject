30bad45d79d1e929166e859e955d0b295bec9e4c
Mohamed Hashim <mahmed@mobiquityinc.com>
2022-09-09 02:04:56 +0200

    Initial Commit

---
+++ ImagesProject/ImagesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesProjectApp.swift
+//  ImagesProject
+//
+//  Created by Mohamed Ahmed on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
